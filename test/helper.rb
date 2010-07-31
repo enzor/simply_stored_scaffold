96fc6ceaa67268818c3215904b5b37f6297ef3fe
Vincenzo Rivello <vincenzo.rivello@gmail.com>
2010-08-01 00:05:46 +0200

    Initial commit to simply_stored_scaffold.

---
+++ test/helper.rb
@@ -0,0 +1,10 @@
+require 'rubygems'
+require 'test/unit'
+require 'shoulda'
+
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'simply_stored_scaffold'
+
+class Test::Unit::TestCase
+end
